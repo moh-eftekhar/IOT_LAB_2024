5fbe5ea28d873e9857be3271b9b171d8abe9a80b
moeftekhar <mohammad.eftekharipour@studenti.polito.it>
2024-10-29 10:07:00 +0100

    update

--- LAB1/lab1_exe3.py
+++ LAB1/lab1_exe3.py
@@ -1,4 +1,5 @@
 import json
+import datetime as dt
 
 with open("catalog.json", "r") as f:
     catalog = json.load(f)
@@ -28,6 +29,40 @@ def searchByserice(servicename):
     return result
 
 
+def searchByMeasurement(measure_Type):
+    result = []
+    devices = catalog.get("devicesList",[])
+    for device in devices:
+        if measure_Type in device.get("measureType",[]):
+            result.append(device)
+    
+    return result
+
+
+def insertDevice(newDevice):
+    devices = catalog.get("devicesList",[])
+    existin_device = None
+    for device in devices:
+        if device.get("deviceID") == newDevice.get("deviceID"):
+            existin_device = device
+            break
+
+            
+    if existin_device:
+        update = input(f"Device with ID {newDevice.get('deviceID')} already exists. Do you want to update it? (y/n)")
+        if update == "y":
+            existin_device.update(newDevice)
+            existin_device["lastUpdate"] = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"Device with ID {newDevice.get('deviceID')} has been updated")
+        else:
+            newDevice["lastUpdate"] = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            devices.append(newDevice)
+            print(f"Device with ID {newDevice.get('deviceID')} has been added")
+
+
+    
+
+    
 
 # Device_name = searchByname("VCNL4010")
 # print(json.dumps(Device_name, indent=4))
@@ -35,5 +70,8 @@ def searchByserice(servicename):
 # Device_id = searchById(1)
 # print(json.dumps(Device_id, indent=4))
 
-Device_service = searchByserice("MQTT")
-print(json.dumps(Device_service, indent=4))
+# Device_service = searchByserice("MQTT")
+# print(json.dumps(Device_service, indent=4))
+
+Measure_Service = searchByMeasurement("Proximity")
+print(json.dumps(Measure_Service, indent=4))

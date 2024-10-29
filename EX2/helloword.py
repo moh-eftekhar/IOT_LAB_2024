4a3c8158459c46dcec3eb3275cbd12337516402e
moeftekhar <mohammad.eftekharipour@studenti.polito.it>
2024-10-29 14:35:24 +0100

    update

---
+++ EX2/helloword.py
@@ -0,0 +1,27 @@
+import cherrypy
+
+class HelloWorld:
+    exposed = True
+    def GET(self,*uri,**params):
+        output = "Hello World!"
+        if len(uri) != 0:
+            output += "<br>uri: " + ',' .join(uri)
+        
+        if params != {}:
+            output += "<br>params: " + ','.join(params)
+        
+        return output
+    
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True
+        }
+    }
+
+    webService = HelloWorld()
+    cherrypy.tree.mount(webService, '/', conf)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
+    

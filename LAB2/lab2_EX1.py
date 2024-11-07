ad4eee1fce4365c506d7833ca9fdca093b121ac2
moeftekhar <mohammad.eftekharipour@studenti.polito.it>
2024-11-07 15:49:18 +0100

    update

--- LAB2/lab2_EX1.py
+++ LAB2/lab2_EX1.py
@@ -17,14 +17,60 @@ class Webcalculator:
             if len(operand) > 0:
                 i=0
                 for i in range(len(operand)):
-                    operand[i] = int(operand[i])
+                    operand[i] = float(operand[i])
             if operation == "add":
                 text_to_return=self.add(operation,operand)
+                return text_to_return
+            elif operation == "sub":
+                text_to_return=self.sub(operation,operand)
+                return text_to_return
+            elif operation == "mul":
+                text_to_return=self.mul(operation,operand)
+                return text_to_return
+            elif operation == "div":
+                text_to_return=self.div(operation,operand)
                 return text_to_return
 
     def add(self,operation,operand):
         print("operand: ",operand)
-        result = operand[0] + operand[1]
+        for i in range(len(operand)):
+            if i == 0:
+                result = operand[i]
+            else:
+                result += operand[i]
+        result_json = {"operation": operation, "operands": operand, "result": result}
+        return json.dumps(result_json, indent=4)
+    
+    def sub(self,operation,operand):
+        print("operand: ",operand)
+        for i in range(len(operand)):
+            if i == 0:
+                result = operand[i]
+            else:
+                result -= operand[i]
+        result_json = {"operation": operation, "operands": operand, "result": result}
+        return json.dumps(result_json, indent=4)
+    
+    def mul(self,operation,operand):
+        print("operand: ",operand)
+        for i in range(len(operand)):
+            if i == 0:
+                result = operand[i]
+            else:
+                result *= operand[i]
+        result_json = {"operation": operation, "operands": operand, "result": result}
+        return json.dumps(result_json, indent=4)
+    
+    def div(self,operation,operand):
+        print("operand: ",operand)
+        for i in range(len(operand)):
+            if i == 0:
+                result = operand[i]
+            else:
+                if operand[i] == 0:
+                    result = "Division by zero is not allowed."
+                    break
+                result /= operand[i]
         result_json = {"operation": operation, "operands": operand, "result": result}
         return json.dumps(result_json, indent=4)
         
